f9d78046f7fa5741d5beb488dd216e724757d99c
ibmpariwesh <pariweshg@gmail.com>
2022-06-01 10:52:38 +0530

    config-server

--- product-Service/src/main/java/com/product/ProductVO.java
+++ product-Service/src/main/java/com/product/ProductVO.java
@@ -13,5 +13,53 @@ public class ProductVO {
 	int ratings;
 	float price;
 	boolean isActive;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBigImage() {
+		return bigImage;
+	}
+	public void setBigImage(String bigImage) {
+		this.bigImage = bigImage;
+	}
+	public String getThumbnail() {
+		return thumbnail;
+	}
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getShortdescription() {
+		return shortdescription;
+	}
+	public void setShortdescription(String shortdescription) {
+		this.shortdescription = shortdescription;
+	}
+	public int getRatings() {
+		return ratings;
+	}
+	public void setRatings(int ratings) {
+		this.ratings = ratings;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	} 
 
 }
