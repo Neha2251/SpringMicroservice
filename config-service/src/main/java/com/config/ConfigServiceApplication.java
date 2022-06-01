ccc5f3b19d712c64353b38d8e4860ae87c00fb2a
ibmpariwesh <pariweshg@gmail.com>
2022-06-01 12:09:32 +0530

    config-server

--- config-service/src/main/java/com/config/ConfigServiceApplication.java
+++ config-service/src/main/java/com/config/ConfigServiceApplication.java
@@ -2,11 +2,13 @@ package com.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
